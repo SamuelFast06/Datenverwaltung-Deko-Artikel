24148a882e4d9532e9b2eacc933b064e2aacf3ff
Paul Sobczak <lightpaul1337@gmail.com>
2023-11-07 10:06:24 +0100

    sadhasods

---
+++ src/main/java/Classes/ChooseManagementFrame.java
@@ -0,0 +1,9 @@
+package Classes;
+
+import javax.swing.*;
+
+public class ChooseManagementFrame {
+    private JButton createManagementButton;
+    private JButton btnJoinManagement;
+    private JLabel lbCreateName;
+}
