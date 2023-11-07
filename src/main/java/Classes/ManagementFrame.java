9886e2912fc330e56553ca1db1d9bd3882e232af
Paul Sobczak <lightpaul1337@gmail.com>
2023-11-07 10:09:08 +0100

    adasd

--- src/main/java/Classes/ManagementFrame.java
+++ src/main/java/Classes/ManagementFrame.java
@@ -3,8 +3,6 @@ package Classes;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.UUID;
 
 
 public class ManagementFrame extends JFrame{
