5e678006adc62ed2003edaca4844fe4f9ca4f237
Paul Sobczak <lightpaul1337@gmail.com>
2023-11-07 12:32:30 +0100

    dwada

--- src/main/java/Classes/ChooseManagementFrame.java
+++ src/main/java/Classes/ChooseManagementFrame.java
@@ -1,9 +1,68 @@
 package Classes;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.ArrayList;
 
-public class ChooseManagementFrame {
-    private JButton createManagementButton;
+public class ChooseManagementFrame extends JFrame{
+    private JPanel chooseManagementPanel;
+    private JButton btnCreateManagement;
     private JButton btnJoinManagement;
+    private JTextField tfCreateName;
+    private JTextField tfJoinID;
     private JLabel lbCreateName;
+    private JLabel lbJoinID;
+    private JLabel lbMessageCreate;
+    private JLabel lbMessageJoin;
+    private JLabel lbHeadline;
+
+    private User user;
+
+
+    public ChooseManagementFrame(User iuser){
+        user = iuser;
+        setContentPane(chooseManagementPanel);
+        setLocation(0,0);
+        setSize(400,280);
+        setVisible(true);
+        setResizable(false);
+        btnManager();
+    }
+
+    public void btnManager(){
+        btnCreateManagement.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String newManagementName = tfCreateName.getText();
+                try {
+                    ManagementController.createManagement(newManagementName, user);
+                    lbMessageJoin.setText("Created " + newManagementName);
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+
+        btnJoinManagement.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String joinID = tfJoinID.getText();
+                try {
+                    Data data = ManagementController.getDataManagement(joinID);
+                    data.addUser(user);
+                    lbMessageJoin.setText("Joined " + data.getName());
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+    }
+
+    public static void main(String[] pablo){
+        User testUser = new User();
+        ChooseManagementFrame frame = new ChooseManagementFrame(testUser);
+    }
 }
