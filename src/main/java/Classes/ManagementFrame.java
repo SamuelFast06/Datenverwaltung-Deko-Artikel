3d3cecb1e5cb18a0dcd7efef1faf55e7de98f192
Samuel Fast <samuel.fast@gmx.de>
2023-11-07 10:09:58 +0100

    The Login Frame is now logging in about 2 sec. The RegisterFrame is creating a new Management and the ManagementFrame is showing the correct name.

--- src/main/java/Classes/ManagementFrame.java
+++ src/main/java/Classes/ManagementFrame.java
@@ -12,18 +12,20 @@ public class ManagementFrame extends JFrame{
     private JButton btnArticles;
     private JButton btnCustomers;
     private JButton btnContactPersons;
-    private JTextArea textArea;
     private JLabel lbManagementName;
     private JLabel lbCurrentUser;
     private JLabel lbMessage;
+
+    Informationtype tappedType = Informationtype.noType;
 
     private Data data;
 
     private User user;
 
     public ManagementFrame(User iuser, Data data){
+        this.data = data;
         setContentPane(managementPanel);
-        user = iuser;
+        this.user = iuser;
         this.data = data;
         lbCurrentUser.setText(user.username);
         lbManagementName.setText(data.getName());
@@ -68,4 +70,5 @@ public class ManagementFrame extends JFrame{
     public static void main(String[] args){
         ManagementFrame management = new ManagementFrame(new User(), new Data());
     }
+
 }
