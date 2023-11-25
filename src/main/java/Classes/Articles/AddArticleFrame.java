e6ee55e2db723096b1796cabbdd54152143b51d9
Paul Sobczak <lightpaul1337@gmail.com>
2023-11-25 20:07:08 +0100

    WeightEinheiten + UI erstellt

--- src/main/java/Classes/Articles/AddArticleFrame.java
+++ src/main/java/Classes/Articles/AddArticleFrame.java
@@ -5,6 +5,9 @@ import Classes.Data;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.util.Arrays;
 import java.util.UUID;
 
 public class AddArticleFrame extends JFrame{
@@ -45,7 +48,7 @@ public class AddArticleFrame extends JFrame{
 
     //Other
     private JTextPane tfDescription;
-    private JScrollBar scrollBar1;
+    private JComboBox comboBox;
 
     private Data data;
     private ArticlesFrame parent;
@@ -84,6 +87,21 @@ public class AddArticleFrame extends JFrame{
                 dispose();
             }
         });
+
+        String[] weightUnits = {"mg","g","kg","t","Stk."};
+        for(int i = 0; i < weightUnits.length; i++){
+            comboBox.addItem(weightUnits[i]);
+        }
+
+        comboBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
     }
 
 }
